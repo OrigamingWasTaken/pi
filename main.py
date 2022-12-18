ed526fca850519fa433dcd8974dc2a03e7603d63
xMarlonB <marlon.barleycorn@edu-vd.ch>
2022-12-18 13:11:45 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import pathlib
+import tkinter as tk
+import random
+import tkinter.font as TkFont
+from tkinter import *
+import os
+
+file = pathlib.Path("./messages.txt")
+lines = file.open().read().split('\n')
+
+message = lines[random.randrange(0,len(lines))]
+
+colors = ["white", "red", "green", "blue", "cyan", "magenta"]
+root = tk.Tk()
+root.configure(bg="white")
+root.title("Message of the Day")
+#root.geometry("480x320")
+root.attributes("-fullscreen",True)
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# canvas = tk.Canvas(root, height=screen_height,width=screen_width,bg="black")
+
+# Create label
+l = Label(root, text = message,bg="black",fg=colors[random.randrange(0,5)], height=480,width=320)
+l.config(font =("Courier", 40))
+l.pack()
+
+# frame = tk.Frame(root,bg="white")
+# frame.place(relwidth=0.8,relheight=0.8)
+
+root.mainloop()
