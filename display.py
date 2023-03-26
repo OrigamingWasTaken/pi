838d37877a031ed88073988aa4be2bb8e80c48db
OrigamingWasTaken <mabarleycorne@gmail.com>
2023-03-26 16:43:33 +0200

    Changed how everything works

---
+++ display.py
@@ -0,0 +1,106 @@
+from tkinter import *
+import tkinter as tk
+import os
+import time
+import random
+import json
+import datetime
+from datetime import date
+from PIL import Image, ImageTk
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
+#Get the special messages
+with open("./special.json") as specialFile:
+    dates = json.load(specialFile)
+
+#Set the message
+def getMessage():
+    with open("./messages.txt","r+") as file:
+        lines = file.readlines()
+        if len(lines) == 0:
+            with open("messages.txt","w+") as o_f:
+                b_lines = open("messages_save.txt","r").readlines()
+                o_f.writelines(b_lines)
+                lines = b_lines
+                o_f.close()
+        message = lines[0]
+        del lines[0]
+    with open("./messages.txt","w+") as file:
+        file.writelines(lines)
+    return(message)
+
+today = str(date.today())[5:]
+
+#Check if today is a special date, if yes then display the correct message
+message = getMessage()
+try:
+    message = dates[date]
+except Exception:
+    pass
+
+#Make a random color for the text, that isn't too bright so it can be read on a white bg
+def random_color():
+        rand = lambda: random.randint(1, 200)
+        return '#%02X%02X%02X' % (rand(), rand(), rand())
+
+r = lambda: random.randint(0,255)
+color = random_color()
+
+#Print check for the above values
+
+print("The message is: " + message)
+print("The color is: " + color)
+
+while True:
+    latestDate = today
+    today = str(date.today())[5:]
+
+    root = tk.Tk()
+    root.configure(bg="white")
+    root.title("Message of the Day")
+    root.attributes("-fullscreen",True)
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    Grid.columnconfigure(root, index=0, weight=1)
+    Grid.rowconfigure(root, 0, weight=1)
+
+    label = Label(root, text=message, bg="white", fg=color, wraplength=root.winfo_width())
+
+    # Position the image label and text label on top of each other using the place geometry manager
+    label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    # Resize the image using the Image.resize method
+
+    # Update the image displayed by the image label
+
+    # Add the widgets to the root window using the grid geometry manager
+    label.grid(row=0,column=0,sticky="nsew")
+
+    def update_label_text(event):
+        # Get the width and height of the window
+        width = event.width
+        height = event.height
+
+        # Calculate the font size based on the width and height of the window
+        font_size = int(min(width, height) / 10)
+
+        # Update the font size of the label
+        label.config(font=("Helvetica", font_size),wraplength=width)
+
+    def update_message():
+        today = datetime.date.today()
+        label.config(text=getMessage())
+
+        tomorrow = today + datetime.timedelta(days=1)
+        midnight = datetime.datetime.combine(tomorrow, datetime.time.min)
+        #root.after(5000,update_message)
+        root.after((midnight - datetime.datetime.now()).seconds * 1000, update_message)
+
+    update_message()
+    
+    root.bind("<Configure>", update_label_text)
+    root.mainloop()
--- main.py
+++ main.py
@@ -7,10 +7,23 @@ import os
 import time
 from datetime import date
 import json
+import glob
+from PIL import Image, ImageTk
 
 if os.environ.get('DISPLAY','') == '':
     print('no display found. Using :0.0')
     os.environ.__setitem__('DISPLAY', ':0.0')
+
+images = glob.glob("images/*")
+image = Image.open(random.choice(images))
+
+# Create a transparent overlay with the same size as the image
+overlay = Image.new('RGBA', image.size, (255, 255, 255, 51))  # 51 is the transparency level (0-255)
+
+# Paste the overlay onto the image
+image.alpha_composite(overlay)
+
+# Create a PhotoImage object from the image
 
 file = pathlib.Path("./messages.txt")
 lines = file.open().read().split('\n')
@@ -44,9 +57,24 @@ screen_height = root.winfo_screenheight()
 Grid.columnconfigure(root, index=0, weight=1)
 Grid.rowconfigure(root, 0, weight=1)
 
-label = Label(root,text=message, bg="white",fg=color,wraplength=root.winfo_width())
+image = ImageTk.PhotoImage(image)
+
+imageLabel = Label(image=image)
+label = Label(root, text=message, bg="white", fg=color, wraplength=root.winfo_width())
+
+# Position the image label and text label on top of each other using the place geometry manager
+imageLabel.place(x=0, y=0, relwidth=1, relheight=1)
+label.place(x=0, y=0, relwidth=1, relheight=1)
+
+# Resize the image using the Image.resize method
+image = image.resize((imageLabel.winfo_width(), imageLabel.winfo_height()))
+
+# Update the image displayed by the image label
+imageLabel.configure(image=image)
+
+# Add the widgets to the root window using the grid geometry manager
 label.grid(row=0,column=0,sticky="nsew")
-label.pack(side="top", fill="both", expand=True)
+imageLabel.grid(row=1,column=0,sticky="nsew")
 
 def update_label_text(event):
     # Get the width and height of the window
@@ -58,6 +86,6 @@ def update_label_text(event):
 
     # Update the font size of the label
     label.config(font=("Helvetica", font_size),wraplength=width)
-
+ 
 root.bind("<Configure>", update_label_text)
 root.mainloop()
